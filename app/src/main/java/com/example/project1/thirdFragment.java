9fe051232e5d7d41990835df2f7a7de8830814a8
DellE7450 <l201047@lhr.nu.edu.pk>
2022-10-18 19:00:27 +0500

    third fragment

---
+++ app/src/main/java/com/example/project1/thirdFragment.java
@@ -0,0 +1,11 @@
+package com.example.project1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class thirdFragment extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
